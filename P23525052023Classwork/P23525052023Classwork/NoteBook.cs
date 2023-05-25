225d8af8a6d731b29a168d1e356eb63279ec17a6
Hamid V. Mammadov <Hamid.mammadov@code.local>
2023-05-25 17:37:10 +0400

    Second Commit

---
+++ P23525052023Classwork/P23525052023Classwork/NoteBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P23525052023Classwork
+{
+    internal class NoteBook
+    {
+        public string Brand;
+        public string Model;
+        public double Price;
+
+        public NoteBook(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}\nModel: {Model}\nPrice: {Price}");
+        }
+    }
+}
---
+++ P23525052023Classwork/P23525052023Classwork/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace P23525052023Classwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NoteBook noteBook = new NoteBook("Asus","Rog");
+            noteBook.Price = 10;
+            noteBook.ShowInfo();
+        }
+    }
+}
